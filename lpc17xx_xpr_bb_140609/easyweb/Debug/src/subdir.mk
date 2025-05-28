67e9c47dc8d53955a5b7095827199dc9927b132e
piotr-wlazlo <piotrwlazlo@icloud.com>
2025-05-28 15:50:32 +0200

    gowno gowno gowno

--- lpc17xx_xpr_bb_140609/easyweb/Debug/src/subdir.mk
+++ lpc17xx_xpr_bb_140609/easyweb/Debug/src/subdir.mk
@@ -29,7 +29,7 @@ OBJS += \
 src/%.o: ../src/%.c src/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C Compiler'
-	arm-none-eabi-gcc -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC17xx -D__CODE_RED -D__NEWLIB__ -I"/Users/piotrwlazlo/Desktop/studia/4semestr/SystemyWbudowane/lpc17xx_xpr_bb_140609/Lib_CMSISv1p30_LPC17xx/inc" -I"/Users/piotrwlazlo/Desktop/studia/4semestr/SystemyWbudowane/lpc17xx_xpr_bb_140609/Lib_EaBaseBoard/inc" -I"/Users/piotrwlazlo/Desktop/studia/4semestr/SystemyWbudowane/lpc17xx_xpr_bb_140609/Lib_MCU/inc" -O0 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fmerge-constants -fmacro-prefix-map="$(<D)/"= -mcpu=cortex-m3 -mthumb -D__NEWLIB__ -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	arm-none-eabi-gcc -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC17xx -D__CODE_RED -D__NEWLIB__ -I"/Users/piotrwlazlo/Desktop/studia/4semestr/SystemyWbudowane/lpc17xx_xpr_bb_140609/Lib_CMSISv1p30_LPC17xx/inc" -I"/Users/piotrwlazlo/Desktop/studia/4semestr/SystemyWbudowane/lpc17xx_xpr_bb_140609/Lib_EaBaseBoard/inc" -I"/Users/piotrwlazlo/Desktop/studia/4semestr/SystemyWbudowane/lpc17xx_xpr_bb_140609/Lib_MCU/inc" -O0 -g3 -gdwarf-4 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fmerge-constants -fmacro-prefix-map="$(<D)/"= -mcpu=cortex-m3 -mthumb -D__NEWLIB__ -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- odtwarzacz/src/D03.c
+++ odtwarzacz/src/D03.c
@@ -8,25 +8,25 @@
  *   Piotr Wlazło 251662
  ******************************************************************************/
 
-#include "lpc17xx_pinsel.h"		/* konfiguracja pinów */
-#include "lpc17xx_gpio.h"		/* obsługa GPIO */
-#include "lpc17xx_ssp.h"		/* obsługa interfejsu SPI (dla karty SD)*/
-#include "lpc17xx_uart.h"		/* obsługa UART (komunikacja szeregowa) */
-#include "lpc17xx_timer.h"		/* obsługa timera */
-#include "stdio.h"				/* standardowa biblio wejścia wyjścia */
+#include "lpc17xx_pinsel.h"
+#include "lpc17xx_gpio.h"
+#include "lpc17xx_ssp.h"
+#include "lpc17xx_uart.h"
+#include "lpc17xx_timer.h"
+#include "stdio.h"					// standardowa biblio wejścia wyjścia
 #include "lpc17xx_dac.h"
 #include "lpc17xx_rtc.h"
 #include "lpc17xx_clkpwr.h"
 #include "stdbool.h"
 #include "joystick.h"
 
-#include "diskio.h"				/* obsługa systemu plików FAT i nośnika SD */
+#include "diskio.h"					// obsługa systemu plików FAT i nośnika SD
 #include "ff.h"
-#include "oled.h"				/* obsluga OLEDu */
-#include "light.h"				/* biblioteka od czujnika swiatla */
+#include "oled.h"
+#include "light.h"
 #include "rotary.h"
 
-#define UART_DEV LPC_UART3		/* def UART3 jako domyślne urządzenie do komunikacji szeregowej */
+#define UART_DEV LPC_UART3			// def UART3 jako domyślne urządzenie do komunikacji szeregowej
 #define SAMPLE_RATE 8000
 #define DELAY 1000000 / 8000
 
@@ -35,12 +35,10 @@
 #define MAX_NAME_LEN 13
 
 
-static uint8_t buf[64];			/* bufor do danych wysyłanych przez UART */
 static uint8_t songs = 0;
 static char songsList[MAX_SONGS][MAX_NAME_LEN];
 
-/* bufory do wav */
-uint8_t bufor1[BUF_SIZE];
+uint8_t bufor1[BUF_SIZE];			// bufory do wav
 uint8_t bufor2[BUF_SIZE];
 
 volatile bool bufor1_pusty = true;
@@ -50,6 +48,9 @@ volatile int aktualnyBufor = 0;
 uint32_t dataOffset = 0;
 FIL wavFile;
 
+RTC_TIME_Type currentTime;
+uint8_t timeStr[40];
+
 
 void TIMER1_IRQHandler(void) {
     static uint32_t pozycja = 0;
@@ -57,19 +58,16 @@ void TIMER1_IRQHandler(void) {
     if (TIM_GetIntStatus(LPC_TIM1, TIM_MR0_INT)) {
         if (aktualnyBufor == 0) {
             DAC_UpdateValue(LPC_DAC, bufor1[pozycja]);
-
         } else {
             DAC_UpdateValue(LPC_DAC, bufor2[pozycja]);
         }
-
         pozycja++;
 
         if (pozycja >= BUF_SIZE) {
             pozycja = 0;
             if (aktualnyBufor == 0) {
             	bufor1_pusty = true;
-            }
-            else {
+            } else {
             	bufor2_pusty = true;
             }
             aktualnyBufor ^= 1;
@@ -85,12 +83,12 @@ static void init_Timer (void)
   TIM_MATCHCFG_Type Match_Cfg;
 
   Config.PrescaleOption = TIM_PRESCALE_USVAL;
-  Config.PrescaleValue = 1; // czyli 1 mikrosek
+  Config.PrescaleValue = 1; 				// czyli 1 mikrosek
 
   //najpierw włączyć zasilanie
   CLKPWR_SetPCLKDiv (CLKPWR_PCLKSEL_TIMER1, CLKPWR_PCLKSEL_CCLK_DIV_4);
   // Ustawić timer.
-  TIM_Cmd (LPC_TIM1, DISABLE); // to w zasadzie jest niepotrzebne, ponieważ po włączeniu zasilania timer jest nieczynny,
+  TIM_Cmd (LPC_TIM1, DISABLE); 				// to w zasadzie jest niepotrzebne, ponieważ po włączeniu zasilania timer jest nieczynny,
   TIM_Init (LPC_TIM1, TIM_TIMER_MODE, &Config);
 
   Match_Cfg.ExtMatchOutputType = TIM_EXTMATCH_NOTHING;
@@ -98,7 +96,7 @@ static void init_Timer (void)
   Match_Cfg.ResetOnMatch = TRUE;
   Match_Cfg.StopOnMatch = FALSE;
   Match_Cfg.MatchChannel = 0;
-  Match_Cfg.MatchValue = 125; // Czyli 125  us.
+  Match_Cfg.MatchValue = 125; 				// Czyli 125  us.
   TIM_ConfigMatch (LPC_TIM1, &Match_Cfg);
   TIM_Cmd (LPC_TIM1, ENABLE);
   // I odblokować przerwania w VIC
@@ -108,7 +106,6 @@ static void init_Timer (void)
 static void init_rtc(void)
 {
     RTC_Init(LPC_RTC);
-
     RTC_ResetClockTickCounter(LPC_RTC);
     RTC_Cmd(LPC_RTC, ENABLE);
 
@@ -125,12 +122,12 @@ static void init_rtc(void)
     RTC_SetFullTime(LPC_RTC, &RTCTime);
 }
 
-static void init_uart(void)		/* inicjalizacja UART */
+static void init_uart(void)
 {
 	PINSEL_CFG_Type PinCfg;
 	UART_CFG_Type uartCfg;
 
-	/* Initialize UART3 pin connect */		/* konfiguruje UART3 na P0.0 i P0.1 */
+	// Initialize UART3 pin connect			// konfiguruje UART3 na P0.0 i P0.1
 	PinCfg.Funcnum = 2;
 	PinCfg.Pinnum = 0;
 	PinCfg.Portnum = 0;
@@ -138,22 +135,19 @@ static void init_uart(void)		/* inicjalizacja UART */
 	PinCfg.Pinnum = 1;
 	PINSEL_ConfigPin(&PinCfg);
 
-	uartCfg.Baud_rate = 115200;		/* ustawia prędkość transmisji */
-	uartCfg.Databits = UART_DATABIT_8;		/* ustawia 8 bitów danych */
-	uartCfg.Parity = UART_PARITY_NONE;		/* ustawia brak parzystości */
-	uartCfg.Stopbits = UART_STOPBIT_1;		/* ustawia 1 bit stopu */
+	uartCfg.Baud_rate = 115200;				// ustawia prędkość transmisji
+	uartCfg.Databits = UART_DATABIT_8;		// ustawia 8 bitów danych
+	uartCfg.Parity = UART_PARITY_NONE;		// ustawia brak parzystości
+	uartCfg.Stopbits = UART_STOPBIT_1;		// ustawia 1 bit stopu
 
-	UART_Init(UART_DEV, &uartCfg);		/* inicjalizuje UART */
-
-	UART_TxCmd(UART_DEV, ENABLE);		/* włącza transmisję */
-
+	UART_Init(UART_DEV, &uartCfg);			// inicjalizuje UART
+	UART_TxCmd(UART_DEV, ENABLE);			// włącza transmisję
 }
 
-
-static void init_ssp(void)		/* inicjalizacja interfejsu SPI */
+static void init_ssp(void)
 {
-	SSP_CFG_Type SSP_ConfigStruct;		/* deklaracja struktury konfiguracyjej dla SPI */
-	PINSEL_CFG_Type PinCfg;				/* i pinów */
+	SSP_CFG_Type SSP_ConfigStruct;			// deklaracja struktury konfiguracyjej dla SPI
+	PINSEL_CFG_Type PinCfg;					// i pinów
 
 	/*
 	 * Initialize SPI pin connect
@@ -177,14 +171,9 @@ static void init_ssp(void)		/* inicjalizacja interfejsu SPI */
 	PinCfg.Pinnum = 2;
 	PINSEL_ConfigPin(&PinCfg);
 
-	SSP_ConfigStructInit(&SSP_ConfigStruct);		/* inicjalizuje SPI i włącza go (kod do końca) */
-
-	// Initialize SSP peripheral with parameter given in structure above
+	SSP_ConfigStructInit(&SSP_ConfigStruct);		// inicjalizuje SPI i włącza go (kod do końca)
 	SSP_Init(LPC_SSP1, &SSP_ConfigStruct);
-
-	// Enable SSP peripheral
-	SSP_Cmd(LPC_SSP1, ENABLE);
-
+	SSP_Cmd(LPC_SSP1, ENABLE);						// Enable SSP peripheral
 }
 
 static void init_dac (void) {
@@ -208,13 +197,13 @@ static void init_dac (void) {
 	DAC_Init(LPC_DAC);
 
 	// Inicjalizacja GPIO dla kontroli wzmacniacza audio LM4811
-	GPIO_SetDir(0, 1UL<<27u, 1u); // LM4811-clk
-	GPIO_SetDir(0, 1UL<<28u, 1u); // LM4811-up/dn
-	GPIO_SetDir(2, 1UL<<13u, 1u); // LM4811-shutdn
+	GPIO_SetDir(0, 1UL<<27UL, 1UL); 	// LM4811-clk
+	GPIO_SetDir(0, 1UL<<28UL, 1UL); 	// LM4811-up/dn
+	GPIO_SetDir(2, 1UL<<1UL, 1UL); 	// LM4811-shutdn
 
-	GPIO_ClearValue(0, 1UL<<27u); // LM4811-clk
-	GPIO_ClearValue(0, 1UL<<28u); // LM4811-up/dn
-	GPIO_ClearValue(2, 1UL<<13u); // LM4811-shutdn
+	GPIO_ClearValue(0, 1UL<<27UL); 	// LM4811-clk
+	GPIO_ClearValue(0, 1UL<<28UL); 	// LM4811-up/dn
+	GPIO_ClearValue(2, 1UL<<13UL); 	// LM4811-shutdn
 }
 
 void SysTick_Handler(void) {		/* obsługa przerwania SysTick */
@@ -238,13 +227,9 @@ static void init_i2c(void) {
 	PinCfg.Pinnum = 11;
 	PINSEL_ConfigPin(&PinCfg);
 
-	// Initialize I2C peripheral
 	I2C_Init(LPC_I2C2, 100000);
-
-	/* Enable I2C1 operation */
-	I2C_Cmd(LPC_I2C2, ENABLE);
+	I2C_Cmd(LPC_I2C2, ENABLE);		// Enable I2C1 operation
 }
-
 
 static uint8_t parseWavHeader(FIL* file) {
 	uint8_t header[44];
@@ -286,7 +271,7 @@ static uint8_t parseWavHeader(FIL* file) {
         offset++;
     }
 
-    if (offset >= 100U) {
+    if (offset >= 100UL) {
         UART_SendString(UART_DEV, (const uint8_t*)"Missing data chunk\r\n");
         return 0;
     }
@@ -295,28 +280,30 @@ static uint8_t parseWavHeader(FIL* file) {
 
     return 1;
 }
+
 static void changeVolume(uint8_t rotaryDir)
 { /* początkowo clock=0 i UP=1*/
 	if (rotaryDir == ROTARY_RIGHT) {
-		  GPIO_SetValue(0, 1UL<<28u); // up
+		  GPIO_SetValue(0, 1UL<<28UL); 		// up
 	}
 	else if (rotaryDir == ROTARY_LEFT) {
-		  GPIO_ClearValue(0, 1UL<<28u); // down
+		  GPIO_ClearValue(0, 1UL<<28UL); 	// down
 	} else {
 		return;
 	}
 
-	GPIO_SetValue(0, 1UL<<27u);
+	GPIO_SetValue(0, 1UL<<27UL);
 	Timer0_us_Wait(100);
-	GPIO_ClearValue(0, 1UL<<27u);
-	GPIO_SetValue(0, 1UL<<28u); // up
+	GPIO_ClearValue(0, 1UL<<27UL);
+	GPIO_SetValue(0, 1UL<<28UL); // up
 }
+
 void select(uint8_t nameIndex) {
-	oled_putString(0, nameIndex * 8 + 1, (uint8_t*) songsList[nameIndex], OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+	oled_putString(0, (nameIndex * 8) + 1, (uint8_t*) songsList[nameIndex], OLED_COLOR_WHITE, OLED_COLOR_BLACK);
 }
 
 void unselect(uint8_t nameIndex) {
-	oled_putString(0, nameIndex * 8 + 1, (uint8_t*) songsList[nameIndex], OLED_COLOR_BLACK, OLED_COLOR_WHITE);
+	oled_putString(0, (nameIndex * 8) + 1, (uint8_t*) songsList[nameIndex], OLED_COLOR_BLACK, OLED_COLOR_WHITE);
 }
 
 static void changeLight() {
@@ -328,9 +315,7 @@ static void changeLight() {
 	} else {
 		oled_setNormalDisplay();
 	}
-	//Timer0_Wait(100);
 }
-
 
 static void playWavFile(char* filename) {
 	BYTE res;
@@ -338,8 +323,7 @@ static void playWavFile(char* filename) {
 
     res = f_open(&wavFile, filename, FA_READ);
     if (res != FR_OK) {
-        sprintf((char*)buf, "Failed to open file: %s, error: %d\r\n", filename, res);
-        UART_Send(UART_DEV, buf, strlen((char*)buf), BLOCKING);
+    	UART_SendString(UART_DEV,(const uint8_t*)"Failed to open file: %s, error: %d\r\n");
         return;
     }
 
@@ -355,6 +339,7 @@ static void playWavFile(char* filename) {
 
     while (1) {
     	changeLight();
+    	updateTime(&currentTime, timeStr);
     	if (bufor1_pusty) {
     		res = f_read(&wavFile, bufor1, sizeof(bufor1), &bytesRead);
     		bufor1_pusty = false;
@@ -376,14 +361,13 @@ static void playWavFile(char* filename) {
 }
 
 static void init_amp(void){
-	  GPIO_SetDir(0, 1UL<<27u, 1u); // clock
-	  GPIO_SetDir(0, 1UL<<28u, 1u); // up/down
-	  GPIO_SetDir(2, 1UL<<13u, 1u); // shutdown
+	  GPIO_SetDir(0, 1UL<<27UL, 1UL); 	// clock
+	  GPIO_SetDir(0, 1UL<<28UL, 1UL); 	// up/down
+	  GPIO_SetDir(2, 1UL<<13UL, 1UL); 	// shutdown
 
 	  //0 na shutdown, czyli włączenie
-	  GPIO_ClearValue(2, 1<<13);
+	  GPIO_ClearValue(2, 1UL<<13UL);
 }
-
 
 static int chooseSong(uint8_t songIndex)
 {
@@ -397,7 +381,7 @@ static int chooseSong(uint8_t songIndex)
 
 	else if (((joy & JOYSTICK_DOWN) != 0)) {
 		unselect(currentSongIndex);
-		if(currentSongIndex == songs - 1U) {
+		if(currentSongIndex == (songs - 1UL)) {
 			currentSongIndex = 0;
 		}
 		else {
@@ -405,148 +389,118 @@ static int chooseSong(uint8_t songIndex)
 		}
 		select(currentSongIndex);
 	}
-
 	else if ((joy & JOYSTICK_UP) != 0) {
 		unselect(currentSongIndex);
-		if(currentSongIndex == 0U) {
-			currentSongIndex = songs - 1U;
+		if(currentSongIndex == 0UL) {
+			currentSongIndex = songs - 1UL;
 		}
 		else {
 			currentSongIndex--;
 		}
 		select(currentSongIndex);
 	}
+	else {
+		return currentSongIndex;
+	}
 	Timer0_Wait(200);
+
 	return currentSongIndex;
 }
 
 void updateTime(RTC_TIME_Type *currentTime, uint8_t *timeStr) {
     RTC_GetFullTime(LPC_RTC, currentTime);
+
     sprintf((char*)timeStr, "%02d:%02d:%02d",
             currentTime->HOUR,
             currentTime->MIN,
             currentTime->SEC);
+
     oled_putString(0, 49, timeStr, OLED_COLOR_BLACK, OLED_COLOR_WHITE);
 }
 
 int main (void) {
-    DSTATUS stat;		/* zmienne do obsługi systemu plików FAT */
-    DWORD p2;
-    WORD w1;
+    DSTATUS stat;		// zmienne do obsługi systemu plików FAT
     BYTE res;
-    BYTE b1;
     DIR dir;
     FILINFO Finfo;
     FATFS Fatfs[1];
 
-    int i = 0;
-
+    uint8_t i = 0;
     int32_t t = 0;
     uint32_t trim = 0;
+	uint8_t songIndex = 0;
 
     init_uart();
     init_i2c();
-    init_ssp();		/* inicjalizacja SPI i UART */
+    init_ssp();
     init_dac();
     init_rtc();
     init_Timer();
     oled_init();
-    light_init();	/* inicjalizacja czujnika swiatla */
+    light_init();
     init_amp();
     rotary_init();
    	joystick_init();
 
-    UART_SendString(UART_DEV, (const uint8_t*)"MMC/SD example\r\n");		/* wysyła kominikat przez UART */
-
-    SysTick_Config(SystemCoreClock / 100);		/* konfiguruje SysTick i czeka 500s */
+    SysTick_Config(SystemCoreClock / 100);		// konfiguruje SysTick i czeka 500s
     Timer0_Wait(500);
 
-    stat = disk_initialize(0);		/* inicjalizacja karty SD */
+    stat = disk_initialize(0);					// inicjalizacja karty SD
 
-	/* sprawdzamy czy karta SD jest dostępna */
     if (stat & STA_NOINIT) {
-    	UART_SendString(UART_DEV,(const uint8_t*)"MMC: not initialized\r\n");
+    	UART_SendString(UART_DEV,(const uint8_t*)"SD: not initialized\r\n");
     }
 
     if (stat & STA_NODISK) {
-    	UART_SendString(UART_DEV,(const uint8_t*)"MMC: No Disk\r\n");
+    	UART_SendString(UART_DEV,(const uint8_t*)"SD: No Disk\r\n");
     }
 
     if (stat != 0) {
         return 1;
     }
 
-    UART_SendString(UART_DEV,(const uint8_t*)"MMC: Initialized\r\n");
-
-	/* pobiera liczbę sektorow na karcie SD i wysyła przez UART */
-    if (disk_ioctl(0, GET_SECTOR_COUNT, &p2) == RES_OK) {
-        i = sprintf((char*)buf, "Drive size: %d \r\n", p2);
-        UART_Send(UART_DEV, buf, i, BLOCKING);
-    }
-
-	/* pobiera rozmiar sektora */
-    if (disk_ioctl(0, GET_SECTOR_SIZE, &w1) == RES_OK) {
-        i = sprintf((char*)buf, "Sector size: %d \r\n", w1);
-        UART_Send(UART_DEV, buf, i, BLOCKING);
-    }
-
-
-    if (disk_ioctl(0, GET_BLOCK_SIZE, &p2) == RES_OK) {
-        i = sprintf((char*)buf, "Erase block size: %d \r\n", p2);
-        UART_Send(UART_DEV, buf, i, BLOCKING);
-    }
-
-    if (disk_ioctl(0, MMC_GET_TYPE, &b1) == RES_OK) {
-        i = sprintf((char*)buf, "Card type: %d \r\n", b1);
-        UART_Send(UART_DEV, buf, i, BLOCKING);
-    }
-
-    /* montuje system plików FAT */
-    res = f_mount(0, &Fatfs[0]);
+    res = f_mount(0, &Fatfs[0]);				// montuje system plików FAT
     if (res != FR_OK) {
-        i = sprintf((char*)buf, "Failed to mount 0: %d \r\n", res);
-        UART_Send(UART_DEV, buf, i, BLOCKING);
+    	UART_SendString(UART_DEV,(const uint8_t*)"Failed to mount 0: %d \r\n");
         return 1;
     }
 
-    /* otwiera katalog główny karty SD */
-    res = f_opendir(&dir, "/");
+    res = f_opendir(&dir, "/");				    // otwiera katalog główny karty SD
     if (res) {
-        i = sprintf((char*)buf, "Failed to open /: %d \r\n", res);
-        UART_Send(UART_DEV, buf, i, BLOCKING);
+    	UART_SendString(UART_DEV,(const uint8_t*)"Failed to open /: %d \r\n");
         return 1;
     }
-
-    RTC_TIME_Type currentTime;
-    uint8_t timeStr[40];
 
     oled_clearScreen(OLED_COLOR_WHITE);
 
-    /* odczytuje i wypisuje nazwy plików z katalogu głównego */
-    while((songs <= MAX_SONGS) && (i<20)) {
+    // odczytuje i wypisuje nazwy plików z katalogu głównego
+    while((songs <= MAX_SONGS) && (i < 20)) {
 		res = f_readdir(&dir, &Finfo);
 		if ((Finfo.fname[0] == '_') || (Finfo.fattrib & AM_DIR)) {
 			i++;
 			continue;
 		}
-		if ((res != FR_OK) || !Finfo.fname[0]) break;
+		if ((res != FR_OK) || !Finfo.fname[0]) {
+			break;
+		}
 		oled_putString(1,1 + (songs * 8), Finfo.fname, OLED_COLOR_BLACK, OLED_COLOR_WHITE);
 		uint8_t nameLen = strnlen(Finfo.fname, sizeof(Finfo.fname));
-		strncpy(songsList[songs], Finfo.fname, nameLen+1U);
+		strncpy(songsList[songs], Finfo.fname, nameLen+1UL);
 		songs++;
 		i++;
     }
 
-    light_enable();						/* aktywowanie czujnika swiatla */
-    light_setRange(LIGHT_RANGE_4000);	/* ustawienie zakresu swiatla do 4000 luksow */
+    light_enable();
+    light_setRange(LIGHT_RANGE_4000);		// ustawienie zakresu swiatla do 4000 luksow
 
 	RTC_GetFullTime(LPC_RTC, &currentTime);
 
-//	sprintf((char*)timeStr, "%02d-%02d-%04d %02d:%02d:%02d", currentTime.DOM, currentTime.MONTH, currentTime.YEAR, currentTime.HOUR, currentTime.MIN, currentTime.SEC);
-//	oled_putString(0, 49, timeStr, OLED_COLOR_BLACK, OLED_COLOR_WHITE);
-//	oled_horizontalLeftScroll(0x06, 0x07);		/* 6 i 7 bo to numery segmentow ekranu OLED (kazdy segment to 8 pikseli) */
+//	uint8_t szlaczek[20];
+//	sprintf((char*)szlaczek, "**********");
+//	oled_putString(0, 41, szlaczek, OLED_COLOR_BLACK, OLED_COLOR_WHITE);
+//	oled_horizontalLeftScroll(0x05, 0x05);
 
-	uint8_t songIndex = 0;
+
 	select(songIndex);
     while(1) {
     	changeLight();
